5ef8476e76150e5ca2a10de4068899f3476dc29e
zammel199 <32831825+zammel199@users.noreply.github.com>
2017-10-16 14:15:19 +0700

    Add files via upload

---
+++ muzammil.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>COBA GRID</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col-sm-4 col-1">
+<h4>Baris 1</h4>
+	</div>
+<div class="col-sm-4 col-2">
+<h4>Baris 2</h4>
+	</div>
+<div class="col-sm-4 col-3">
+<h4>Baris 3</h4>
+	</div>
+
+</div>
+</div>
+<style> 
+.col-1{
+	background-color: #ff6347;
+}
+.col-2{
+	background-color: #00ff7f;
+}
+.col-3{
+	background-color: #d8bfd8;
+}
+.col-4{
+	background-color: #ee82ee;
+}
+.col-5{
+	background-color: #ee82ee;
+}
+.col-6{
+	background-color: #f5deb3;
+}
+.col-7{
+	background-color: #40e0d0;
+}
+.col-8{
+	background-color: #a9a9a9;
+}
+.col-9{
+	background-color: #e74c3c;
+}
+.col-10{
+	background-color: #8b008b;
+}
+.col-11{
+	background-color: #d2b48c;
+}
+.col-12{
+	background-color: #008080;
+}
+</style>
+</body>
+</html>
